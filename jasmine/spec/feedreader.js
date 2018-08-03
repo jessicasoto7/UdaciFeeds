9f2b4c261764642d8b79a9a824a2ce41757729d7
Jessica Tirado <jessicasoto7@gmail.com>
2018-08-03 10:36:21 -0500

    Wrote test for URL and name from allFeeds object.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,17 +26,30 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
+        
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 
+        it('url defined', function() {
+            allFeeds.forEach( function(feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            })               
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('name defined', function() {
+            allFeeds.forEach( function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            })               
+        });
     });
 
 
